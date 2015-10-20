b18dbc54f1438220049efebb36723d14c1af765e
Sam Martens <sammartens1993@gmail.com>
2015-10-21 00:37:36 +0300

    cycling resolve

--- variable_scope.h
+++ variable_scope.h
@@ -1,12 +1,13 @@
 #ifndef C_CALCULATOR_VARIABLE_SCOPE_H
 #define C_CALCULATOR_VARIABLE_SCOPE_H
 
-#include "calc.h"
 const int VARIABLE_SCOPE_SIZE = 100;
+
+class variable;
 
 class variable_scope {
 public:
-	variable vScope[VARIABLE_SCOPE_SIZE];
+	variable *vScope[VARIABLE_SCOPE_SIZE];
 	int iVarNum = 0;
 	int isExistedVar(char *pVarName);
 	// !Do here increment method
